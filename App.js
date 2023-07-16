14318f517e7f10886d7a062e10247f346143b231
Nitya-Somani <nityasahajisomani@gmail.com>
2023-07-16 16:02:28 +0530

    React | [Super Power- 2 of JSX (Javascript Xml) ]

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// How can i put react element inside react component ?
+//JSX [JavaScript XML ]Super Power - 2 
+//cross side scripting explained - 
 
-//creating React element
+/* What is cross side scripting ? */
+/* 
+Basically whenever a attacker tries to inject  some sort of scripts in user 
+appplication through a click button or any event and then can control the user application then it is known as 
+a security vulnerability as cross side scripting.
 
-const reactElem = <h1>React element here </h1>;
+so even ewhe through an api a mailicious data is coming then also 
+JSX  has a super power it does not directly assigns the data from the api 
+First it checks and sanitize the data from the api then assigns it to JSX .
+*/
 
-//creating React functional component
+//For eg :
 
-const Heading = () => <div id="container">{reactElem}</div>;
+const fetchApi = api.getData(); //getting malacious data through getData();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading/>)
+const heading = () =>
+(
+    <div>
+    <h1>HI THERE CAN JSX PRINT MALICIOUS DATA ?</h1>
+     <p>{ fetchApi }</p>
+    <h2>NO , it can't  JSX has a super power it will sanitize
+         the data and then assign it . Wohooooo !!!</h2>
+    </div>
+)
