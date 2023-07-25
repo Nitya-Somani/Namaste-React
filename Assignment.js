76c45a8525fd4134524c53fcc8487a00017ff86e
Nitya-Somani <nityasahajisomani@gmail.com>
2023-07-26 00:36:07 +0530

    REACT | [Difference between a library and framework ]

--- Assignment.js
+++ Assignment.js
@@ -12,3 +12,25 @@ It has built-in emmet in it .
 Emmet provides us keyboard shortcuts with which a developer life become easier It is like a supercharged assistant to write code with some abbreviations and shortcuts that gets expanded in full fledged code .
 
 */
+
+/*Difference between a library and framework ?*/
+/*
+    Framework:
+
+A framework is a complete software platform that provides a foundation for building applications. It is an integrated set of tools, components, and conventions designed to solve specific types of problems or build particular types of applications.
+The framework dictates the overall structure and flow of the application. It follows the "Inversion of Control" principle, where the control of the application's flow is transferred to the framework. The developer writes code to fit within the framework's architecture and interacts with the framework through predefined hooks or callbacks.
+In other words, the framework decides how the application should be organized and provides a specific architecture to follow.
+Examples of frameworks include Ruby on Rails (for web applications), Django (for web applications), Angular (for building web apps), and React Native (for building cross-platform mobile apps).
+Library:
+
+A library, on the other hand, is a collection of pre-written code or functionalities that can be used to perform specific tasks. It is not a complete software platform but rather a set of tools that can be integrated into an application to solve particular problems or add specific functionalities.
+Unlike frameworks, libraries do not dictate the overall application structure or flow. The developer retains control over the application and decides when and how to use the library's functionalities.
+Libraries provide a more selective approach, allowing developers to use only the specific parts of the library that they need in their application. They are generally more flexible and can be integrated into existing projects with greater freedom.
+Examples of libraries include jQuery (for DOM manipulation and AJAX), NumPy (for numerical computing in Python), Gson (for JSON serialization/deserialization in Java), and lodash (for utility functions in JavaScript).
+In summary, a framework provides a complete software platform with predefined architecture and flow, while a library offers a set of tools or functionalities that developers can use selectively and retain control over the application's structure and flow. The choice between using a framework or a library depends on the project's requirements and whether you need a complete solution with a defined structure (framework) or specific functionalities to enhance an existing project (library).
+
+*/
+
+
+
+
