c2c722090053dd5006362391d1792abd4e3ff646
Nitya-Somani <nityasahajisomani@gmail.com>
2023-07-26 00:22:21 +0530

    React|[what is emmet ?]

---
+++ Assignment.js
@@ -0,0 +1,14 @@
+//Resolving Assignment - 1
+/* What is emmet ? */
+/*
+Emmet is powerful tool that is used by web developers to write thier code more quickly 
+and efficiently . Emmet uses abbreviations and shortcuts and turn them into full fledge code .
+for eg: if you want to write a div tag then manually you will have to type the opening and closing tag but with emmet if you type "div" then emmet automatically expands the code into full structure - <div></div>.
+It also gives abbreviations and shortcuts for ids, classes , etc and also we can use emmet with HTML , HAML , JSX etc .
+Emmet helps us to write a lightning fast code  .
+for eg : It has a abbreaviation for lorem ipsum text so that saves a lot of time and a developer will not have to look up and search for the text again .
+Emmet can be used with IDE (Integrated development environment) like visual studio code 
+It has built-in emmet in it .
+Emmet provides us keyboard shortcuts with which a developer life become easier It is like a supercharged assistant to write code with some abbreviations and shortcuts that gets expanded in full fledged code .
+
+*/
