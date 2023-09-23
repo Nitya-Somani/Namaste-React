119132cf601034a8b88073400bc39e9b539787c1
Nitya Somani <116200890+Nitya-Somani@users.noreply.github.com>
2023-09-23 20:41:47 +0530

    Update commonLinks.js

--- src/utils/commonLinks.js
+++ src/utils/commonLinks.js
@@ -5,7 +5,7 @@ export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const DISH_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.7051457&lng=75.8559729&restaurantId="
+export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
 
 // use this link its working properly
 // https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING
